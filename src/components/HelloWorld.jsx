37806378991d92d465c76412742679ab99991c94
Paola.Zarate <Paola.Zarate@wnco.com>
2020-04-30 18:44:43 -0500

    Add component Header
    Paola Zarate - Geek girl

--- src/components/HelloWorld.jsx
+++ src/components/HelloWorld.jsx
@@ -1,6 +1,6 @@
-import React from "react";
-import "../assets/styles/App.scss";
+import React from 'react';
+import '../assets/styles/App.scss';
 
-const HelloWorld = () => (<h1>Hola</h1>);
+const HelloWorld = () => <h1>Hola</h1>;
 
 export default HelloWorld;
---
+++ src/components/Search.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Search = () => (
+    <section class="main">
+        <h2 class="main__title">¿Qué quieres ver hoy?</h2>
+        <input type="text" class="input" placeholder="Buscar...">
+    </section>
+);
+
+export default Search;
+_
+
+

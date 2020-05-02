1e2f2e15e677d0fe9a222426f36de556f7cb3c95
Paola.Zarate <Paola.Zarate@wnco.com>
2020-05-01 20:28:05 -0500

    Add components an scss files
    Paola Zarate - Geek girl

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,10 +1,26 @@
 import React from 'react';
+import Carousel from '../components/Carousel';
+import CarouselItem from '../components/CarouselItem';
+import Category from '../components/Category';
+import Footer from '../components/Footer';
 import Header from '../components/Header';
+import Search from '../components/Search';
+
 import '../assets/styles/App.scss';
 
 const App = () => (
   <div className="App">
     <Header />
+    <Search />
+
+    <Category>
+      <Carousel>
+        <CarouselItem />
+        <CarouselItem />
+        <CarouselItem />
+      </Carousel>
+    </Category>
+    <Footer />
   </div>
 );
 
